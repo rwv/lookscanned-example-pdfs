f26470eb18f3d76919bfe21a728e89a40226cacf
Seedgou <i@zczc.cz>
2024-12-04 10:19:49 +0800

    feat: add fonts

--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -8,16 +8,32 @@ import { computed } from 'vue'
 const route = useRoute()
 const lang = route.params.lang
 
+const fonts = {
+  'zh-CN': {
+    'font-family': 'Noto Serif SC',
+    'font-optical-sizing': 'auto',
+    'font-style': 'normal',
+  },
+}
+
 const markdownHTML = computed(() => {
   console.log(lang)
   // get markdown
   const markdown = zhCNMarkdown
   return DOMPurify.sanitize(parse(markdown))
 })
+
+const style = computed(() => {
+  return fonts[lang]
+})
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@200..900&display=swap');
+</style>
 
 <template>
   <main>
-    <div v-html="markdownHTML"></div>
+    <div v-html="markdownHTML" :style="style"></div>
   </main>
 </template>
